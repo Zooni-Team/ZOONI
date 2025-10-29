d80998d39bab3741267adc0dacb4e38781995823
theotrosman <theotrosman@gmail.com>
2025-10-28 22:59:03 -0300

    Finalización de conejos principales + Add ciudades

--- obj/Debug/net8.0/Zooni.AssemblyInfo.cs
+++ obj/Debug/net8.0/Zooni.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zooni")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41c0cedb5abad813d8febaa5903271e4b6fa6abd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53756c56cfe33d3e544543006118c060a4215d51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zooni")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zooni")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
