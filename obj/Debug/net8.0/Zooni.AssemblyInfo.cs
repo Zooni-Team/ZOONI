759afae82504923ad8b267ab02fcd1fde9f03727
THEO VALENTÍN TROSMAN <49092919@ort>
2025-10-30 12:10:56 -0300

    camara mascota

--- obj/Debug/net8.0/Zooni.AssemblyInfo.cs
+++ obj/Debug/net8.0/Zooni.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zooni")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f237dd07c82047645329ac47189d14d23e78387")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d9a31c6c8a04e68f28c735c59fdff3eeaba8432")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zooni")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zooni")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
